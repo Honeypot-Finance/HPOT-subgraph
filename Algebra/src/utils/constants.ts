a21fb15bb2345be5b3a070d5a79c5ae9504f0637
rabbitblood <qz154772610@gmail.com>
2024-10-30 14:12:41 -0700

    init

--- Algebra/src/utils/constants.ts
+++ Algebra/src/utils/constants.ts
@@ -4,7 +4,7 @@ import { Factory as FactoryContract } from '../types/templates/Pool/Factory'
 
 
 export const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000'
-export const FACTORY_ADDRESS = '0xDfCC62B3eE40EE453Cb2788576B56e746FC79827'
+export const FACTORY_ADDRESS = '0xab49321DF952315E208a2B7046A00d2015E39cba'
 export const FEE_DENOMINATOR = BigDecimal.fromString('1000000') 
 
 export let ZERO_BI = BigInt.fromI32(0)
